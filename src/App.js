71cddf95458cec074c2698b5543738d4d70673f1
ramyy33 <164842712+ramyy33@users.noreply.github.com>
2024-08-26 20:28:47 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,179 @@
+import { useRef, useState } from 'react';
+import './App.css';
+import Notfound from './Notfound';
+
+
+
+function App() {
+  
+
+  const [currentMusicDetails, setCurrentMusicDetails] = useState({
+    songName: 'Chasing',
+    songArtist: 'NEFFEX',
+    songSrc: './Assets/songs/Chasing - NEFFEX.mp3',
+    songAvatar: './Assets/Images/image1.jpg'
+    
+  })
+  
+
+  
+  const [audioProgress, setAudioProgress] = useState(0);
+  const [isAudioPlaying, setIsAudioPlaying] = useState(false);
+  const [musicIndex, setMusicIndex] = useState(0);
+  const [musicTotalLength, setMusicTotalLength] = useState('04 : 38');
+  const [musicCurrentTime, setMusicCurrentTime] = useState('00 : 00');
+  
+
+  const currentAudio = useRef()
+
+  const handleMusicProgressBar = (e)=>{
+    setAudioProgress(e.target.value);
+    currentAudio.current.currentTime = e.target.value * currentAudio.current.duration / 100;
+  }
+
+  
+  let avatarClass = ['objectFitCover','objectFitContain','none']
+  const [avatarClassIndex, setAvatarClassIndex] = useState(0)
+  const handleAvatar = ()=>{
+    if (avatarClassIndex >= avatarClass.length - 1) {
+      setAvatarClassIndex(0)
+    }else{
+      setAvatarClassIndex(avatarClassIndex + 1)
+    }
+  }
+
+
+  
+  const handleAudioPlay = ()=>{
+    if (currentAudio.current.paused) {
+      currentAudio.current.play();
+      setIsAudioPlaying(true)
+    }else{
+      currentAudio.current.pause();
+      setIsAudioPlaying(false)
+    }
+  }
+
+  const musicAPI = [
+    {
+      songName: 'Chasing',
+      songArtist: 'NEFFEX',
+      songSrc: './Assets/songs/Chasing - NEFFEX.mp3',
+      songAvatar: './Assets/Images/image1.jpg'
+    },
+    {
+      songName: 'Hai sa ne iubim ca nemtii',
+      songArtist: 'Dani Mocanu',
+      songSrc: './Assets/songs/Dani Mocanu - Hai sa ne iubim ca nemții  Official Video.mp3',
+      songAvatar: './Assets/Images/11b954731c653e9282d15939dd141bea.1000x1000x1.png'
+    },
+    {
+      songName: 'Hot now',
+      songArtist: 'NBA YOUNGBOY',
+      songSrc: './Assets/songs/YoungBoy Never Broke Again - Hot Now [Official Audio].mp3',
+      songAvatar: './Assets/Images/369afe88f604eba1cc44f9cb55ac50c7.1000x1000x1.jpg'
+    },
+    {
+      songName: 'BIG BEN',
+      songArtist: 'MGK666',
+      songSrc: './Assets/songs/MGK666 - BIG BEN  BUZZ HOUSE OST.mp3',
+      songAvatar: './Assets/Images/download.jpg'
+    },
+
+    {
+      songName: 'Couldve been me',
+      songArtist: 'Quano Rondo',
+      songSrc: './Assets/songs/Quando Rondo - Couldve Been Me (Official Music Video).mp3',
+      songAvatar: './Assets/Images/quando-rondo-couldve-been-me-1200x675.jpg'
+    }
+    
+  ]
+
+  const handleNextSong = ()=>{
+    if (musicIndex >= musicAPI.length - 1) {
+      let setNumber = 0;
+      setMusicIndex(setNumber);
+      updateCurrentMusicDetails(setNumber);
+    }else{
+      let setNumber = musicIndex + 1;
+      setMusicIndex(setNumber)
+      updateCurrentMusicDetails(setNumber);
+    }
+  }
+
+  const handlePrevSong = ()=>{
+    if (musicIndex === 0) {
+      let setNumber = musicAPI.length - 1;
+      setMusicIndex(setNumber);
+      updateCurrentMusicDetails(setNumber);
+    }else{
+      let setNumber = musicIndex - 1;
+      setMusicIndex(setNumber)
+      updateCurrentMusicDetails(setNumber);
+    }
+  }
+
+  const updateCurrentMusicDetails = (number)=>{
+    let musicObject = musicAPI[number];
+    currentAudio.current.src = musicObject.songSrc;
+    currentAudio.current.play();
+    setCurrentMusicDetails({
+      songName: musicObject.songName,
+      songArtist: musicObject.songArtist,
+      songSrc: musicObject.songSrc,
+      songAvatar: musicObject.songAvatar
+    })
+    setIsAudioPlaying(true);
+  }
+
+  const handleAudioUpdate = ()=>{
+    
+    let minutes = Math.floor(currentAudio.current.duration / 60);
+    let seconds = Math.floor(currentAudio.current.duration % 60);
+    let musicTotalLength0 = `${minutes <10 ? `0${minutes}` : minutes} : ${seconds <10 ? `0${seconds}` : seconds}`;
+    setMusicTotalLength(musicTotalLength0);
+
+    
+    let currentMin = Math.floor(currentAudio.current.currentTime / 60);
+    let currentSec = Math.floor(currentAudio.current.currentTime % 60);
+    let musicCurrentT = `${currentMin <10 ? `0${currentMin}` : currentMin} : ${currentSec <10 ? `0${currentSec}` : currentSec}`;
+    setMusicCurrentTime(musicCurrentT);
+
+    const progress = parseInt((currentAudio.current.currentTime / currentAudio.current.duration) * 100);
+    setAudioProgress(isNaN(progress)? 0 : progress)
+  }
+
+
+
+  return (
+    <>
+    <div className="container">
+      <audio src='./Assets/songs/Chasing - NEFFEX.mp3' ref={currentAudio} onEnded={handleNextSong} onTimeUpdate={handleAudioUpdate}></audio>
+      <div className="blackScreen"></div>
+      <div className="music-Container">
+        <p className='musicPlayer'>Music Player</p>
+        <p className='music-Head-Name'>{currentMusicDetails.songName}</p>
+        <p className='music-Artist-Name'>{currentMusicDetails.songArtist}</p>
+        <img src={currentMusicDetails.songAvatar} className={avatarClass[avatarClassIndex]} onClick={handleAvatar} alt="song Avatar" id='songAvatar'/>
+        <div className="musicTimerDiv">
+          <p className='musicCurrentTime'>{musicCurrentTime}</p>
+          <p className='musicTotalLenght'>{musicTotalLength}</p>
+        </div>
+        <input type="range" name="musicProgressBar" className='musicProgressBar' value={audioProgress} onChange={handleMusicProgressBar} />
+        <div className="musicControlers">
+          <i className='fa-solid fa-backward musicControler' onClick={handlePrevSong}></i>
+          <i className={`fa-solid ${isAudioPlaying? 'fa-pause-circle' : 'fa-circle-play'} playBtn`} onClick={handleAudioPlay}></i>
+          <i className='fa-solid fa-forward musicControler' onClick={handleNextSong}></i>
+        </div>
+      </div>
+<div>
+
+</div>
+
+    </div>
+    </>
+
+  );
+}
+
+export default App;
---
+++ src/Notfound.js
@@ -0,0 +1,9 @@
+
+export default function Notfound() {
+  return (
+    <div>
+<h2>Page not found!</h2>
+
+    </div>
+  )
+}
